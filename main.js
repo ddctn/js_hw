1b00dd7e5bf535207067e260305e181b594bcf41
ddctn <avociafuture@gmail.com>
2025-01-15 16:44:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// 1 задача
+
+const str = "abce";
+console.log(str[0]);
+console.log(str[1]);
+console.log(str[3]);
+
+// 2 задача
+
+let num1 = parseInt(prompt("введите первое число", ""));
+let num2 = parseInt(prompt("введите второе число", ""));
+console.log(num1 > 0 || num2 > 0);
+
+// 3 задача
+
+let age = parseInt(prompt("введите свой возраст", ""));
+if (age >= 18) {
+  console.log("вы совершеннолетний :)");
+} else {
+  console.log("вы несовершеннолетний :(");
+}
